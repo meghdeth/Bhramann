461b3a2d233648c198da326ad4a890d74241d3d2
Amit Roy <amitroy.thedev@gmail.com>
2025-07-07 12:25:55 +0530

    added swap input in bus search

--- frontend/src/components/Home/searchTabs/BusSearch.jsx
+++ frontend/src/components/Home/searchTabs/BusSearch.jsx
@@ -1,16 +1,27 @@
 import DatePicker from "react-datepicker";
+import { ArrowRightLeft } from 'lucide-react'
 
 export default function BusSearchFields({ values, setValues }) {
   const { from, to, date } = values;
 
   return (
     <>
-      <div className="flex gap-4 mb-4">
+      <div className="flex gap-4 mb-4 items-center">
         <input
           placeholder="From"
           value={from}
           onChange={(e) => setValues({ ...values, from: e.target.value })}
           className="input-base"
+        />
+        <ArrowRightLeft
+          className="size-10 cursor-pointer"
+          onClick={() =>
+            setValues({
+              ...values,
+              from: to,
+              to: from,
+            })
+          }
         />
         <input
           placeholder="To"
